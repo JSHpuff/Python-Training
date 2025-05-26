664d641e5106bae9d7cb931f4b4c2483fd814e4e
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-05-26 13:52:04 +0800

    update tkinter 04 set widgets 1

---
+++ tkinter/section_01_Tkinter_Fundamentals/04_Set_Widgets_1.py
@@ -0,0 +1,20 @@
+# Using the widget constructor when creating the widget
+import tkinter as tk
+from tkinter import ttk
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Testing Setting Widgets")
+
+    window_width = 600
+    window_height = 600
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    center_x = int(screen_width / 2 - window_width / 2)
+    center_y = int(screen_height / 2 - window_height / 2)
+    root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
+    root.resizable(False, False)
+    root.iconbitmap("./000_testing_imgs/test_icon.ico")
+
+    ttk.Label(root, text="Testing text for ttk").pack()
+    root.mainloop()
