69035e876818667a1b716f46826a2abdb3666955
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-05-27 15:19:00 +0800

    upload the tkinter 8 button 2

---
+++ tkinter/section_01_Tkinter_Fundamentals/08_Button_2.py
@@ -0,0 +1,32 @@
+# Tkinter image button example
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+
+def handle_click():
+    showinfo(
+        title="Information",
+        message="Download button clicked!"
+    )
+
+if __name__ == "__main__":
+    # Main Window
+    root = tk.Tk()
+    root.title("Image Button Demo")
+    window_width = 300
+    window_height = 200
+    root.geometry(f"{window_width}x{window_height}")
+    root.resizable(False, False)
+    root.iconbitmap("./000_testing_imgs/test_icon.ico")
+
+    download_icon = tk.PhotoImage(file="./000_testing_imgs/download.png")
+    download_button = ttk.Button(
+        root,
+        image=download_icon,
+        command=handle_click
+    )
+
+    download_button.pack(ipadx=5, ipady=5, expand=True)
+
+    root.mainloop()
