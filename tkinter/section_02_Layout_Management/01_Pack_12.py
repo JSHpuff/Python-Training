bb871737361de991a19c6fa09eef00fbd444694b
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-05-28 11:00:01 +0800

    upload tkinter 2 layout 1 pack 12

---
+++ tkinter/section_02_Layout_Management/01_Pack_12.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import ttk
+
+class MainApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Tkinter Pack Layout")
+        self.root.geometry("300x200")
+
+        self.build_ui()
+    
+    def build_ui(self):
+        name_label = ttk.Label(self.root, text="Name: ")
+        name_entry = ttk.Entry(self.root)
+        button = ttk.Button(self.root, text="Submit")
+
+        name_label.pack(side=tk.LEFT)
+        name_entry.pack(side=tk.LEFT, expand=True, fill=tk.X)
+        button.pack(side=tk.LEFT)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainApp(root)
+    root.mainloop()
