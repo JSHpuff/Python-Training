a36ad50f46dcf5cc243ff95ab5967a86b5f88ce5
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-05-28 11:02:52 +0800

    upload tkinter 2 layout 1 pack 13

---
+++ tkinter/section_02_Layout_Management/01_Pack_13.py
@@ -0,0 +1,27 @@
+# Internal paddings: ipadx & ipady
+
+import tkinter as tk
+
+class MainApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Tkinter Pack Layout")
+        self.root.geometry("600x400")
+
+        self.build_ui()
+    
+    def build_ui(self):
+        label1 = tk.Label(self.root, text="Pack", bg="red", fg="white")
+        label2 = tk.Label(self.root, text="Pack", bg="green", fg="white")
+        label3 = tk.Label(self.root, text="Pack", bg="blue", fg="white")
+        label4 = tk.Label(self.root, text="Pack", bg="purple", fg="white")
+
+        label1.pack(side=tk.LEFT)
+        label2.pack(side=tk.LEFT, ipadx=40)
+        label3.pack(side=tk.LEFT, ipady=40)
+        label4.pack(side=tk.LEFT, ipadx=80, ipady=80)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainApp(root)
+    root.mainloop()
