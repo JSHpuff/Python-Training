ea094776f536234248247f10012bd328ab1f11cd
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-06-06 16:43:24 +0800

    update tkinter 3 widgets 12 spinbox 2

---
+++ tkinter/section_03_Ttk_Tkinter_Widgets/12_Spinbox_2.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.geometry("300x200")
+root.resizable(False, False)
+root.title("Spinbox Demo")
+
+current_value = tk.StringVar()
+spin_box = ttk.Spinbox(
+    root,
+    from_=0,
+    to=50,
+    values=(0, 10, 20, 30 ,40, 50),
+    textvariable=current_value,
+    wrap=True
+)
+
+spin_box.pack()
+
+root.mainloop()
