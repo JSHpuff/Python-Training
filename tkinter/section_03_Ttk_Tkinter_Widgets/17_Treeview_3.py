23e965fb8cc7640800684d80277a2cd6aff39e53
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-06-09 09:54:12 +0800

    update tkinter 3 widgets 17 treeview 3

---
+++ tkinter/section_03_Ttk_Tkinter_Widgets/17_Treeview_3.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import ttk
+
+class MainApp():
+    def __init__ (self, root):
+        self.root = root
+        self.root.title("Tkinter Treeview")
+        self.root.geometry("400x300")
+        self.treeview_ui()
+    
+    def treeview_ui(self):
+        self.treeview = ttk.Treeview()
+        level1 = self.treeview.insert("", tk.END, text="San Jose")
+        self.treeview.insert(level1, tk.END, text="John Doe")
+        self.treeview.insert(level1, tk.END, text="Jane Doe")
+        self.treeview.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
+        
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainApp(root)
+    root.mainloop()
