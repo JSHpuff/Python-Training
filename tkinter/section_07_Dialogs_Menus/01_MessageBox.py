765d23db59195c5d249a2cd3305ddd83dc42d387
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-06-20 16:52:11 +0800

    update tkinter 7 Dialogs menus 1 message box

---
+++ tkinter/section_07_Dialogs_Menus/01_MessageBox.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showerror, showwarning, showinfo
+
+# Create thte root window
+root = tk.Tk()
+root.title("Tkinter MessageBox")
+root.resizable(False, False)
+root.geometry("350x150")
+
+options = {"fill": "both", "padx": 10, "pady": 10, "ipadx": 5}
+
+ttk.Button(
+    root,
+    text="Show an error message",
+    command=lambda: showerror(
+        title="Error",
+        message="This is an error message."
+    )
+).pack(**options)
+
+ttk.Button(
+    root,
+    text="Show an information message",
+    command=lambda: showinfo(
+        title="Information",
+        message="This is an information message."
+    )
+).pack(**options)
+
+ttk.Button(
+    root,
+    text="Show an warning message",
+    command=lambda: showwarning(
+        title="Warning",
+        message="This is a warning message."
+    )
+).pack(**options)
+
+root.mainloop()
