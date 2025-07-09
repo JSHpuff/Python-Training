27079a5739029e834e4ccc7502cc9f846cb3274e
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-07-09 11:46:05 +0800

    update tkinter 8 tkinter theme stles 3 ttk elements 01

---
+++ tkinter/section_08_Tkinter_Themes_Styles/03_ttkElements_01.py
@@ -0,0 +1,16 @@
+# Introduction to ttk elements
+import tkinter as tk
+from tkinter import ttk
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        style = ttk.Style(self)
+
+        layout = style.layout('TLabel')
+        print(layout)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
