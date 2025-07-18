280c4a1840d53ee6a6f43cb57b70d91a5ee403cb
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-07-18 16:29:50 +0800

    update tkinter 01 01 3 set width height postion

---
+++ tkinter/section_01_01_Tkinter_BuildWindow/03_setWidthHeightPos.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('HI HOw are you')
+width = 600
+height = 400
+left = 0
+top = 0
+root.geometry(f"{width}x{height}+{left}+{top}")
+
+root.mainloop()
