f422d3ad0301e8a44e3568375b34ddbf92c71af3
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-07-23 13:43:26 +0800

    update tkinter 26 menu 5 enable

---
+++ tkinter/section_26_Menu/05_Enable.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('Enable and Disable')
+root.geometry('200x200')
+
+def open():
+    print('open')
+    menubar.entryconfig('Save', state='normal')
+
+def save():
+    print('save')
+
+def exit():
+    print('exit')
+    menubar.entryconfig('Save', state='disabled')
+
+menu = tk.Menu(root)
+menubar = tk.Menu(menu)
+menubar.add_command(label='Open', command=open, state='normal')
+menubar.add_command(label='Save', command=save, state='disabled')
+menubar.add_command(label='Exit', command=exit)
+menu.add_cascade(label='File', menu=menubar)
+
+root.config(menu=menu)
+
+root.mainloop()
