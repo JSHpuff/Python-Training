03d947c642da0e991e1f1b64429b2787a13e9508
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-07-28 10:17:07 +0800

    update basic 1 fundamental 1 snippet

---
+++ basic/section_1_Fundamentals/01_Snippet.py
@@ -0,0 +1,11 @@
+# Define main function to print out something
+def main():
+    i = 1
+    max = 10
+    while (i < max):
+        print(i)
+        i += 1
+
+# Call function main
+if __name__ == "__main__":
+    main()
