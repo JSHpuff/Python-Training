e222262bef090d98416e7212934c8781ad054efe
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-08-05 17:16:53 +0800

    update OOP 1 class and object 13 encapsulation

---
+++ OOP/section_1_ClassObject/13_Encapsulation.py
@@ -0,0 +1,16 @@
+class Counter:
+    def __init__(self):
+        self.current = 0
+    def increment(self):
+        self.current += 1
+    def value(self):
+        return self.current
+    def reset(self):
+        self.current = 0
+
+counter = Counter()
+counter.increment()
+counter.increment()
+counter.current = -999
+
+print(counter.value())
