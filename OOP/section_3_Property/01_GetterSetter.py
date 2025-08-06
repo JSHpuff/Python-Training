b6ce653ec7f86e5ecc046c1dea8a0eecaddac5ab
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-08-06 09:47:37 +0800

    update

--- OOP/section_3_Property/01_GetterSetter.py
+++ OOP/section_3_Property/01_GetterSetter.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 class Person:
     def __init__(self, name, age):
         self.name = name
@@ -11,3 +13,12 @@ class Person:
     def get_age(self):
         return self._age
     
+    age = property(fget=get_age, fset=set_age)
+
+print(Person.age)
+
+john = Person('John', 18)
+pprint(john.__dict__)
+
+john.age = 19
+pprint(Person.__dict__)
