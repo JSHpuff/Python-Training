5335bc805a5ef029b9f73f4b2d59533376357e81
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-08-06 10:08:25 +0800

    update OOP 3 property 4 delete property

---
+++ OOP/section_3_Property/04_DeleteProperty.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+
+class Person:
+    def __init__(self, name):
+        self._name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if value.strip() == '':
+            raise ValueError('name cannot be empty')
+        self._name = value
+
+    @name.deleter
+    def name(self):
+        del self._name
+        
