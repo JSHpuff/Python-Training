cd478411f9282af9638daa9f2f827fa8626e2a3b
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-08-06 13:19:34 +0800

    update basic 8 strings 3 transform

---
+++ basic/section_8_Strings/03_Transform.py
@@ -0,0 +1,43 @@
+# str(x) string
+a = str(123)
+b = str([1, 2, 3])
+c = str(True)
+d = str(False)
+print(a)
+print(b)
+print(b[0])
+print(c)
+print(d)
+print(c+d)
+
+# ascii(x) ASCII
+a = ascii('Hello')
+print(a)
+
+# bin(x) 2
+a = bin(1234)
+print(a)
+
+# oct(x) 8
+a = oct(1234)
+print(a)
+
+# hex(x) 16
+a = hex(1234)
+print(a)
+
+# chr(x) Unicode
+a = chr(101)
+b = chr(202)
+c = chr(9999)
+print(a)
+print(b)
+print(c)
+
+# ord(x) Reverse Unicode
+a = ord('e')
+b = ord('E')
+c = ord('s')
+print(a)
+print(b)
+print(c)
