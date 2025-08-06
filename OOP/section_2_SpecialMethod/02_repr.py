5758ade26a6338fcb661d101fb0d8a3b64c3d94f
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-08-06 08:26:33 +0800

    update OOP 2 special method 2 repr

---
+++ OOP/section_2_SpecialMethod/02_repr.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def __repr__(self):
+        return f'Person("{self.first_name}", "{self.last_name}", {self.age})'
+    
+    def __str__(self):
+        return f'({self.first_name}, {self.last_name}, {self.age})'
+    
+person = Person('John', 'Doe', 25)
+print(person)   # str()
+print(repr(person)) # repr()
