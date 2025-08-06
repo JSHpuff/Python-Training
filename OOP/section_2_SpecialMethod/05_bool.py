8faa2ecc5a5b85cae5f52a849c257a755c15ea47
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-08-06 08:57:52 +0800

    update OOP 2 special method 5 bool

---
+++ OOP/section_2_SpecialMethod/05_bool.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __bool__(self):
+        if self.age < 18 or self.age > 65:
+            return False
+        return True
+
+person = Person('Jane', 16)
+print(bool(person))
