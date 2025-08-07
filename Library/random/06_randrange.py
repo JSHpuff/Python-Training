65954b2a36b582aed0b60a9389c9a42b825ce676
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-08-07 08:52:06 +0800

    update Library random 6 randrange

---
+++ Library/random/06_randrange.py
@@ -0,0 +1,10 @@
+import random
+
+# random.randrange(x, y, step)
+# step default set to 1
+# random x to y with step=step
+print(random.randrange(2, 10, 2))
+
+# random.randrange(x)
+# 0 to x random integer
+print(random.randrange(10))
