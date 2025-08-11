86e76e04cdb5cae16b6770fa65479cd57ef1766f
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-08-11 09:08:43 +0800

    update libarary datetime 5 datetime 2

---
+++ Library/datetime/05_datetime_2.py
@@ -0,0 +1,5 @@
+import datetime
+
+print(datetime.datetime.today())    # 2021-10-19 06:15:46.022925
+print(datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=8))))
+# 2021-10-19 14:15:46.027982+08:00
