cc689788be255030f350e5622daaabce4278d82b
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-08-11 09:02:59 +0800

    update

--- Library/datetime/04_time_3.py
+++ Library/datetime/04_time_3.py
@@ -2,12 +2,12 @@ import datetime
 
 thisTime = datetime.time(14, 0, 0, 1, tzinfo=datetime.timezone(datetime.timedelta(hours=8)))
 
-print(thisTime)
-print(thisTime.isoformat())
-print(thisTime.tzname())
-print(thisTime.strftime('%H:%M:%S'))
+print(thisTime)                         # 14:00:00.000001+08:00
+print(thisTime.isoformat())             # 14:00:00.000001+08:00
+print(thisTime.tzname())                # UTC+08:00
+print(thisTime.strftime('%H:%M:%S'))    # 14:00:00
 
 today = datetime.date.today()
 newTime = today.replace(hour=20)
 
-print(newTime)
+print(newTime)  # 20:00:00.000001+08:00
