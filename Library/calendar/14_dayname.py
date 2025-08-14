014fef25659d07b61b3afa658e818d44913199ab
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-08-14 13:49:17 +0800

    update library calendar 14 day name

---
+++ Library/calendar/14_dayname.py
@@ -0,0 +1,4 @@
+# calendar.day_name、calendar.day_abbr 使用後會回傳星期一到星期天的名稱或縮寫。
+import calendar
+print(list(calendar.day_name))
+# ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
