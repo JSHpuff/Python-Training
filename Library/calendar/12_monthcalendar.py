0123b61d768f8e91ce9ed64499e452951431e7ad
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-08-14 13:46:50 +0800

    update library calendar 12 month calendar

---
+++ Library/calendar/12_monthcalendar.py
@@ -0,0 +1,5 @@
+# calendar.monthcalendar(year, month) 可以回傳一個日曆的二維串列。
+import calendar
+
+print(calendar.monthcalendar(2021, 10))
+# [[0, 0, 0, 0, 1, 2, 3], [4, 5, 6, 7, 8, 9, 10], [11, 12, 13, 14, 15, 16, 17]...
