a1098937c7c85f4788a3836d20cde9ecab31db53
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-08-14 13:26:11 +0800

    update library calendar 7 is leap

---
+++ Library/calendar/07_isleap.py
@@ -0,0 +1,7 @@
+# calendar.isleap(year) 可以判斷某一年是否為閏年，回傳 True 或 False。
+
+import calendar
+
+print(calendar.isleap(2020))    # True
+print(calendar.isleap(2021))    # False
+print(calendar.isleap(2022))    # False
